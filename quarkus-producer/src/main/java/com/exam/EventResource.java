b5c994f4c0306d1acd9ce5ec7b101ab3bf221901
roberto.nav20 <roberto.nav20@gmail.com>
2024-07-12 01:52:59 +0200

    Add open api

--- quarkus-producer/src/main/java/com/exam/EventResource.java
+++ quarkus-producer/src/main/java/com/exam/EventResource.java
@@ -1,11 +1,19 @@
 package com.exam;
 
 import java.util.concurrent.CompletionStage;
+
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
+
 import com.exam.model.Event;
+
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
@@ -13,23 +21,21 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/event")
-@RolesAllowed({"quarkus-exam-role"})
+@RolesAllowed({ "quarkus-exam-role" })
 public class EventResource {
 
     @Channel("events")
     Emitter<Event> eventEmitter;
 
-    @Operation(
-        summary = "Publish Event",
-        description = "Publish an event to kafka"
-    )
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Event published", content = {
+                    @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(type = SchemaType.OBJECT, implementation = Event.class)) }),
+    })
+    @Operation(summary = "Publish Event", description = "Publish an event to kafka")
     @POST
     @Path("/produce")
     @Consumes(MediaType.APPLICATION_JSON)
-    @Counted(
-        name = "event-produced-api",
-        description = "Number of event produced via api"
-    )
+    @Counted(name = "event-produced-api", description = "Number of event produced via api")
     public CompletionStage<Void> produce(Event event) {
         return eventEmitter.send(event);
     }
